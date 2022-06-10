ebc67406765488d2f202066371b662303b74d869
Vedant Sheoran <vedantsheoran01@gmail.com>
2022-06-10 11:12:27 +0530

    Create Insertion.java

---
+++ Insertion.java
@@ -0,0 +1,16 @@
+// concept of insertion sort 
+
+int[] arr = {7, 8, 3, 2, 1};
+
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        printArray(arr);
+    }
+}
